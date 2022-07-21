5a9d9e208f9a33ec4186579949601af78d43d83b
Rodrigo <rodrigo.elias@engenharia.ufjf.br>
2022-07-21 16:22:56 -0300

    Layout do menu

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout << "Ola" << endl;
+    cout << "<< MENU >>" << endl << endl;
+    
+    cout << "[1] Fecho transitivo direto do vértice" << endl << "Parâmetro: Id de um vértice de um grafo direcionado" << endl << endl;
+    
+    cout << "[2] Fecho transitivo indireto do vértice" << endl << "Parâmetro: Id de um vértice de um grafo direcionado" << endl << endl;
+    
+    cout << "[3] Caminho mínimo entre dois vértices usando algoritmo de Djkstra" << endl << "Parâmetro: dois Ids de vértices" << endl << endl;
+    
+    cout << "[4] Caminho mínimo entre estes dois vértices usando algoritmo de Floyd" << endl << "Parâmetro: dois IDs de vértices do grafo"  << endl << endl;
+    
+    cout << "[5] Árvore Geradora Mínima sobre o subgrafo vértice-induzido por X usando o algoritmo de Prim" << endl << "Parâmetro: um subconjunto X de vértices do grafo" << endl << endl;
+    
+    cout << "[6] Árvore Geradora Mínima sobre o subgrafo vértice-induzido por X usando o algoritmo de Kruskal" << endl << "Parâmetro: um subconjunto X de vértices do grafo" << endl << endl;
+    
+    cout << "[7] Árvore dada pela ordem de caminhamento em profundidade a partir de nó dado em parâmetro, destacando as arestas de retorno" << endl << "Parâmetro: um ID de vértice" << endl << endl;
+    
+    cout << "[8] Ordenação topológica em D ou a informação de que D não é um grafo acíclico direcionado" << endl << "Parâmetro: o próprio grafo acíclico direcionado D" << endl << endl;
+    
+    cout << "[0] SAIR" << endl;
 
     return 0;
 }
